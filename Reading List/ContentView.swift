e6e630c7df528862acc5d3efeb6a038e3b1e10b5
Henry Majewski <henryjmajewski@icloud.com>
2021-08-02 11:36:06 -0400

    Initial Commit

---
+++ Reading List/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Reading List
+//
+//  Created by Henry Majewski on 8/2/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Reading List/Reading_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Reading_ListApp.swift
+//  Reading List
+//
+//  Created by Henry Majewski on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Reading_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
